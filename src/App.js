96e9442876321f46bdba481754e90ad4e4e2a3ad
jacksonvfo <jacksonvfo@gmail.com>
2020-01-24 14:54:05 -0300

    integration with backend

--- src/App.js
+++ src/App.js
@@ -1,148 +1,49 @@
 import React, { useState, useEffect } from "react";
+import api from './services/api';
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
-
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
 
 
 function App() {
-  const [github_username, setGithub_username] = useState('');
-  const [techs, setTechs] = useState('');
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
+  const [devs, setDevs] = useState([]);
+
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
+    async function loadDevs() {
+      const response = await api.get('/devs');
 
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (err) => {
-        console.log(err);
-      },
-      {
-        timeout: 30000,
-      }
-    )
+      setDevs(response.data);
+    }
 
-  }, [])
+    loadDevs();
+  }, []);
 
-  async function handleAddDev(e){
-    e.preventDefault();
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
+
+    setDevs([...devs, response.data]);
   }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do GitHub</label>
-            <input
-              name="github_username"
-              id="github_username"
-              required
-              value={github_username}
-              onChange={e => setGithub_username(e.target.value)}
-                />
-          </div>
-  
-          <div className="input-block">
-              <label htmlFor="techs">Tecnologias</label>
-              <input
-                name="techs"
-                id="techs"
-                required
-                value={techs}
-                onChange={e => setTechs(e.target.value)}
-                  />
-          </div>
-    
-          <div className="input-group">
-                <div className="input-block">
-                  <label htmlFor="latitude">Latitude</label>
-                  <input
-                    type="number"
-                    name="latitude"
-                    id="latitude"
-                    required
-                    value={latitude}
-                    onChange={e => setLatitude(e.target.value)}
-                  />
-                </div>
-                <div className="input-block">
-                  <label htmlFor="longitude">Longitude</label>
-                  <input
-                    type="number"
-                    name="longitude"
-                    id="longitude"
-                    required
-                    value={longitude}
-                    onChange={e => setLongitude(e.target.value)}
-                  />
-                </div>
-              </div>
-
-              <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev}/>
 
       </aside >
-          <main>
-            <ul>
-              <li className="dev-item">
-                <header>
-                  <img src="https://scontent.ftow1-1.fna.fbcdn.net/v/t1.0-9/p960x960/34561825_1536935399750049_9166154422407921664_o.jpg?_nc_cat=108&_nc_ohc=jOL8ILlyWFYAX-UAi0J&_nc_ht=scontent.ftow1-1.fna&_nc_tp=1002&oh=375eb976dd274a1d963e1fd076818472&oe=5E9E3509"></img>
-                  <div className="user-info">
-                    <strong>Bilubilu Bilu</strong>
-                    <span>Basic, Cobol, Assembly</span>
-                  </div>
-                </header>
-                <p>Bio bio bio bio bio bio bio bio bio bio</p>
-                <a href="github.com/jacksonvfo">Acessar perfil no Github</a>
-              </li>
-
-              <li className="dev-item">
-                <header>
-                  <img src="https://scontent.ftow1-1.fna.fbcdn.net/v/t1.0-9/p960x960/34561825_1536935399750049_9166154422407921664_o.jpg?_nc_cat=108&_nc_ohc=jOL8ILlyWFYAX-UAi0J&_nc_ht=scontent.ftow1-1.fna&_nc_tp=1002&oh=375eb976dd274a1d963e1fd076818472&oe=5E9E3509"></img>
-                  <div className="user-info">
-                    <strong>Bilubilu Bilu</strong>
-                    <span>Basic, Cobol, Assembly</span>
-                  </div>
-                </header>
-                <p>Bio bio bio bio bio bio bio bio bio bio</p>
-                <a href="github.com/jacksonvfo">Acessar perfil no Github</a>
-              </li>
-
-              <li className="dev-item">
-                <header>
-                  <img src="https://scontent.ftow1-1.fna.fbcdn.net/v/t1.0-9/p960x960/34561825_1536935399750049_9166154422407921664_o.jpg?_nc_cat=108&_nc_ohc=jOL8ILlyWFYAX-UAi0J&_nc_ht=scontent.ftow1-1.fna&_nc_tp=1002&oh=375eb976dd274a1d963e1fd076818472&oe=5E9E3509"></img>
-                  <div className="user-info">
-                    <strong>Bilubilu Bilu</strong>
-                    <span>Basic, Cobol, Assembly</span>
-                  </div>
-                </header>
-                <p>Bio bio bio bio bio bio bio bio bio bio</p>
-                <a href="github.com/jacksonvfo">Acessar perfil no Github</a>
-              </li>
-
-              <li className="dev-item">
-                <header>
-                  <img src="https://scontent.ftow1-1.fna.fbcdn.net/v/t1.0-9/p960x960/34561825_1536935399750049_9166154422407921664_o.jpg?_nc_cat=108&_nc_ohc=jOL8ILlyWFYAX-UAi0J&_nc_ht=scontent.ftow1-1.fna&_nc_tp=1002&oh=375eb976dd274a1d963e1fd076818472&oe=5E9E3509"></img>
-                  <div className="user-info">
-                    <strong>Bilubilu Bilu</strong>
-                    <span>Basic, Cobol, Assembly</span>
-                  </div>
-                </header>
-                <p>Bio bio bio bio bio bio bio bio bio bio</p>
-                <a href="github.com/jacksonvfo">Acessar perfil no Github</a>
-              </li>
-            </ul>
-          </main>
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
     </div>
-        );
-      }
-      
-      export default App;
+  );
+}
+
+export default App;
